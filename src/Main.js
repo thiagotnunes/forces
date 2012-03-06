70b567665f830138f150744e295d9a87e125c147
Thiago Nunes <thiagotnunes@gmail.com>
2012-03-05 23:23:06 -0300

    Extracting some logic from the main in order to decouple the algorithm from drawing the graph

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,8 @@
-function main(canvas) {
-    var randomizer = locationRandomizer(canvas.width, canvas.height, 200);
-    var creator = binaryNodeCreator(nodesCreator(12, randomizer));
+function main(nodes, drawer) {
     var repulsion = coulombRepulsion();
     var attraction = hookeAttraction();
     var nodeForces = forces(repulsion, attraction);    
     var locationCalculator = forceLocationCalculator(nodeForces);
-
-    var nodes = function(creator) {
-        var nodes = creator.nodes();
-        
-        drawer = nodesDrawer(nodes, canvas);
-        drawer.draw();
-
-        return nodes;
-    }(creator);
 
     var updateLocation = function(nodes) {
         locationCalculator.updateLocationOf(nodes, vector(0, 0));
