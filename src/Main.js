9ca866cc868a68a39feddbbc7857c2956934dbcb
Thiago Nunes <thiagotnunes@gmail.com>
2012-03-08 11:39:42 -0300

    Adding stopping point to the system

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,45 @@
 function main(nodes, drawer) {
-    var repulsion = coulombRepulsion();
-    var attraction = hookeAttraction();
-    var nodeForces = forces(repulsion, attraction);    
-    var locationCalculator = forceLocationCalculator(nodeForces);
+  var repulsion = coulombRepulsion();
+  var attraction = hookeAttraction();
+  var nodeForces = forces(repulsion, attraction);    
+  var locationCalculator = forceLocationCalculator(nodeForces);
+  var STABILIZED = 1;
+  
+  var kinetic_energy_for = function(nodes) {
+    var kinetic_energy = 0;
+    _.each(nodes, function(node) {
+      kinetic_energy += node.velocity.dot(node.velocity);
+    });
 
-    var updateLocation = function(nodes) {
-        locationCalculator.updateLocationOf(nodes, vector(0, 0));
-        drawer.draw();
-        setTimeout(callback, 100);
-        function callback() {
-            updateLocation(nodes);
-        };
+    return kinetic_energy;
+  };
+
+  var isSystemStable = function(kinetic_energy) {
+    return kinetic_energy < STABILIZED;
+  };
+
+  var updateLocation = function(nodes) {
+    locationCalculator.updateLocationOf(nodes, vector(0, 0));
+    drawer.draw();
+
+    kinetic_energy = kinetic_energy_for(nodes);
+
+    var callback = function() {
+      updateLocation(nodes, kinetic_energy);
     };
 
-    var organize = function() {
-        updateLocation(nodes);
-    };
+    if (isSystemStable(kinetic_energy)) {
+      alert("Done.");
+    } else {
+      setTimeout(callback, 10);
+    }
+  };
 
-    return {
-      organize: organize
-    };
+  var organize = function() {
+    updateLocation(nodes);
+  };
+
+  return {
+    organize: organize
+  };
 }
